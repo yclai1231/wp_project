50aafcd17dc1482f1cd90f56d21713ba5e098cb3
李昀宸 <b09705015@ntu.edu.tw>
2022-12-31 03:29:21 +0800

    Outlet

--- frontend/src/container/AppFrame.js
+++ frontend/src/container/AppFrame.js
@@ -1,44 +1,56 @@
-import React from 'react'
-import styled from 'styled-components';
-import { useNavigate, useLocation } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { useNavigate, useLocation, Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 const BarContainer = styled.div`
-    height: 25%;
-    position: sticky;
-    background-color: #FAEBD7;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  width: 100%;
+  height: 10%;
+  position: sticky;
+  background-color: #faebd7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-    ul li {
-        list-style: none;
-        display: table;
-        margin: 0 auto;
-      }
-    ul {
-        display: flex;
-    }
-    // ul {
-    //     margin: 3% 30%;
-    //     justify-content: space-between;
-    // }
-    // li {
-    //     list-style: none;
-    //     display: inline-block;
-    //     color: #ccc;
-    // }
+  ul li {
+    list-style: none;
+    display: table;
+    margin: 0 auto;
+  }
+  ul {
+    display: flex;
+  }
+  // ul {
+  //     margin: 3% 30%;
+  //     justify-content: space-between;
+  // }
+  // li {
+  //     list-style: none;
+  //     display: inline-block;
+  //     color: #ccc;
+  // }
 `;
 
 const AppFrame = () => {
-    const navigate = useNavigate();
-    return (
-        <BarContainer>
-            <ul>
-                <li onclick={() => navigate('/products')}>產品總覽</li>
-                <li onclick={() => navigate('/vip')}>會員專區</li>
-                <li onclick={() => navigate('/contactUs')}>聯絡我們</li>
-            </ul>
-        </BarContainer>
-    )
-}
-export default AppFrame
+  const navigate = useNavigate();
+  return (
+    <AppContainer>
+      <BarContainer className="123">
+        <ul>
+          <li onClick={() => navigate("/products")}>產品總覽</li>
+          <li onClick={() => navigate("/vip")}>會員專區</li>
+          <li onClick={() => navigate("/contactUs")}>聯絡我們</li>
+        </ul>
+      </BarContainer>
+      <Outlet />
+    </AppContainer>
+  );
+};
+export default AppFrame;
--- frontend/src/container/SignIn.js
+++ frontend/src/container/SignIn.js
@@ -1,17 +1,14 @@
-import LogIn from '../components/LogIn';
+import LogIn from "../components/LogIn";
 const SignIn = () => {
-
-//     const handleLogin = (name) => {
-//     if (!name)
-//         displayStatus({
-//             type: "error",
-//             msg: "Missing user name",
-//         });
-//     else setSignedIn(true);
-//   }
-  return ( 
-        <LogIn />
-  );
-}
+  //     const handleLogin = (name) => {
+  //     if (!name)
+  //         displayStatus({
+  //             type: "error",
+  //             msg: "Missing user name",
+  //         });
+  //     else setSignedIn(true);
+  //   }
+  return <LogIn />;
+};
 
 export default SignIn;
