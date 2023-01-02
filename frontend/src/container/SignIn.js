fd21295dbfa48809eccac05a2f9d653e41228e95
賴羿蓁 <laiyichen@laiyizhendeMacBook-Air.local>
2023-01-02 21:44:59 +0800

    signIn

--- frontend/src/container/SignIn.js
+++ frontend/src/container/SignIn.js
@@ -1,13 +1,5 @@
 import LogIn from "../components/LogIn";
 const SignIn = () => {
-  //     const handleLogin = (name) => {
-  //     if (!name)
-  //         displayStatus({
-  //             type: "error",
-  //             msg: "Missing user name",
-  //         });
-  //     else setSignedIn(true);
-  //   }
   return <LogIn />;
 };
 
