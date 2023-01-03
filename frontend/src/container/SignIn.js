800a62445dc32a3652850de05d5c4b70c2ecde1f
李昀宸 <b09705015@ntu.edu.tw>
2023-01-04 03:56:19 +0800

    21

--- frontend/src/container/SignIn.js
+++ frontend/src/container/SignIn.js
@@ -1,13 +1,15 @@
 import LogIn from "../components/LogIn";
 import React, { useState } from "react";
 import { useWeb } from "./hooks/useWeb";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  const { CRUD } = useWeb();
+  const { CRUD, setLogin, setCustomerID } = useWeb();
   const [data, setData] = useState({});
-  const [login, setLogin] = useState();
+  const [error, setError] = useState(false);
   const [mode, setMode] = useState("1");
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleModeChange = (_, newValue) => {
     setMode(newValue);
@@ -20,18 +22,38 @@ const SignIn = () => {
       [name]: value,
     }));
   };
-
+  const handleClickSubmit = async () => {
+    if (!data.mail || !data.password) {
+      setError(false);
+      if (!data.mail)
+        setError((prev) => ({ ...prev, mail: "Please input a mail." }));
+      if (!data.password)
+        setError((prev) => ({ ...prev, password: "Please input a password." }));
+      return;
+    }
+    setError(false);
+    const result = await CRUD("C", mode === "1" ? "/logIn" : "/signUp")(data);
+    console.log(result);
+    if (result.errors) {
+      setError(result.errors);
+    } else {
+      setLogin(true);
+      setCustomerID(result.customer_id);
+      navigate("/");
+    }
+  };
+  const navigateToForgetPassword = () => navigate("/forget");
   return (
     <LogIn
       data={data}
       mode={mode}
-      // result={result}
-      // setResult={setResult}
+      error={error}
       showPassword={showPassword}
       handleClickShowPassword={handleClickShowPassword}
       handleModeChange={handleModeChange}
       handleInputChange={handleInputChange}
-      submit={CRUD("C", mode === "1" ? "/logIn" : "/signUp")}
+      navigateToForgetPassword={navigateToForgetPassword}
+      submit={handleClickSubmit}
     />
   );
 };
