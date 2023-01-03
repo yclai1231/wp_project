0b2ef28e49c5a4060d17b7e5e0e78f65018ff5ce
李昀宸 <b09705015@ntu.edu.tw>
2023-01-04 00:53:10 +0800

    重設密碼

--- frontend/src/container/Forget.js
+++ frontend/src/container/Forget.js
@@ -3,59 +3,80 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import EmailIcon from "@mui/icons-material/Email";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useWeb } from "./hooks/useWeb";
 
 const WholeContainer = styled.div`
-    width: 100%;
+  width: 100%;
+  display: flex;
+  margin-bottom: 5vmin;
+  align-items: center;
+  justify-content: center;
+  .info {
+    margin-top: 10vmin;
     display: flex;
-    margin-bottom: 5vmin;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
-    .info {
-        margin-top: 10vmin;
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        flex-direction: column;
-    }
-    h1 {
-        border-bottom: 8px solid gray;
-        font-size: 24px;
-        display: inline-block;
-    }
-    .title {
-        margin-bottom: 20px;
-    }
-    .text {
-        margin-top: 1vmin;
-    }
-    .edit {
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        margin-top: 3vmin;
-    }
+    flex-direction: column;
+  }
+  h1 {
+    border-bottom: 8px solid gray;
+    font-size: 24px;
+    display: inline-block;
+  }
+  .title {
+    margin-bottom: 20px;
+  }
+  .text {
+    margin-top: 1vmin;
+  }
+  .edit {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: 3vmin;
+  }
 `;
 
 const Forget = () => {
+  const { CRUD } = useWeb();
+  const [data, setData] = useState({});
+  const Query = CRUD("W", "/password/forgot-password");
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setData({ email: value });
+  };
 
   return (
     <WholeContainer>
-        <div className="info">
-            <div className="title">
-                <h1>忘記密碼</h1>
-            </div>  
-            <div className="input">
-                <p>輸入會員帳號（信箱）</p>
-                <TextField className='text' label="" variant="standard" defaultValue="" type="password" />
-            </div> 
-            <div className="edit">
-                <Button>傳送驗證碼</Button>
-            </div> 
-        </div>  
+      <div className="info">
+        <div className="title">
+          <h1>忘記密碼</h1>
+        </div>
+        <div className="input">
+          <p>輸入會員帳號（信箱）</p>
+          <TextField
+            className="text"
+            label=""
+            variant="standard"
+            defaultValue=""
+            type="password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="edit">
+          <Button
+            onClick={() => {
+              Query(data);
+            }}
+          >
+            傳送驗證碼
+          </Button>
+        </div>
+      </div>
     </WholeContainer>
-    
   );
 };
 
